f355ffe496cb5b259883be6e89f29776ce9b8bfe
PaulTKoenig <paul.koenig.002@gmail.com>
2025-05-02 15:26:41 -0400

    reorder articles

--- frontend/src/lib/articles.ts
+++ frontend/src/lib/articles.ts
@@ -1,14 +1,20 @@
 export const articles = [
     {
         id: "001",
-        title: "how to lose money gambling",
-        date: "1/1/11",
+        title: "MLExpress",
+        date: "4/30/25",
         description: "thgis is about some things i was thinking about"
     },
-    // {
-    //     id: "002",
-    //     title: "is this useless? prolably",
-    //     date: "2/2/22",
-    //     description: "these are more and more thoughts"
-    // }
+    {
+        id: "002",
+        title: "Basketball 'R' Us",
+        date: "2/2/22",
+        description: "these are more and more thoughts"
+    },
+    {
+        id: "003",
+        title: "NCAAB",
+        date: "2/2/22",
+        description: "these are more and more thoughts"
+    }
 ];
