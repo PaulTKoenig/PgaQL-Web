4b941904c21a6e3105cc21c9babb760615288d47
PaulTKoenig <paul.koenig.002@gmail.com>
2025-06-30 16:24:50 -0400

    autosize stat columns in nbaql grid

--- frontend/src/lib/nbaql/Grid.svelte
+++ frontend/src/lib/nbaql/Grid.svelte
@@ -16,8 +16,8 @@
 	export let playerData = new Map();
     let columnDefs: Array<any> = [
         { headerName: 'Player', field: 'player', flex: 1, minWidth: 100 },
-        { headerName: data.x_column_name, field: 'stat1', minWidth: 50 },
-        { headerName: data.y_column_name, field: 'stat2', minWidth: 50 }
+        { headerName: data.x_column_name, field: 'stat1', minWidth: 50, autoSize: true },
+        { headerName: data.y_column_name, field: 'stat2', minWidth: 50, autoSize: true }
     ];
     let rowData: Array<any> = [];
 
