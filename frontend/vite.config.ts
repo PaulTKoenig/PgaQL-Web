aca700856fd785b052226093e4e393daa6810d0d
PaulTKoenig <paul.koenig.002@gmail.com>
2025-06-30 15:26:44 -0400

    add tailwind back to vite config file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: ['paulkoenig.dev']
-  }
+	plugins: [sveltekit(), tailwindcss()],
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+    	allowedHosts: ['paulkoenig.dev']
+	},
 });
